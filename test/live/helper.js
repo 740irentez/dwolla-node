3ee91ad34c0591a34dc5166416cfb3608128d007
Gordon Zheng <technix1@gmail.com>
2014-09-05 16:57:30 -0400

    add live tests for funding sources

--- test/live/helper.js
+++ test/live/helper.js
@@ -3,6 +3,7 @@ module.exports = {
 		url: /http:\/\/.*/,
 		dwollaId: /[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
 		checkoutId: /[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/,
-		checkoutURL: /https:\/\/.*.dwolla.com\/payment\/checkout\/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/
+		checkoutURL: /https:\/\/.*.dwolla.com\/payment\/checkout\/[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}/,
+		ISOTimestamp: /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\dZ/
 	}
 };
